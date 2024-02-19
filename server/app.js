a466b6623be56e61e46fbc98b8049a677fff6914
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-19 16:34:27 +0100

    day 1 completed

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohortsData = require('./cohorts.json');
+const studentsData = require('./students.json');
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -14,7 +16,7 @@ const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(cors({origin: ['http://localhost:5173', 'http://example.com']}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -24,9 +26,21 @@ app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
+app.get("/", (req, res) => {
+  res.json({ message: 'Hello from the route /' });
+});
+
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohortsData);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(studentsData);
 });
 
 
