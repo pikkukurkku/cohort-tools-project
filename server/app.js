8a0424009663d98fca8eeabb617c0abe4ecf4aa4
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-21 14:53:12 +0100

    set up models and routes

--- server/app.js
+++ server/app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const PORT = 5005;
 const cors = require("cors");
-
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohortsData = require('./cohorts.json');
-const studentsData = require('./students.json');
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
 
 
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 
-// MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-app.use(cors({origin: ['http://localhost:5173', 'http://example.com']}));
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5005"] }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -24,27 +19,47 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-app.get("/", (req, res) => {
-  res.json({ message: 'Hello from the route /' });
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongoose-cohort-tools-api-dev")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+ 
 
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the route /" });
+});
 
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts", (req, res) => {
-  res.json(cohortsData);
+app.get("/api/cohorts", cors(), (req, res) => {
+  Cohort.find({})
+  .then((cohorts)=> {
+    console.log("Retrieved cohorts ->", cohorts);
+    res.json(cohorts);
+  })
+  .catch((error) => {
+    console.error("Error while retrieving cohorts ->", error);
+    res.status(500).send({ error: "Failed to retrieve cohorts" });
+  });
 });
 
-app.get("/api/students", (req, res) => {
-  res.json(studentsData);
+app.get("/api/students", cors(), (req, res) => {
+  Student.find({})
+  .then((students)=> {
+    console.log("Retrieved students ->", students);
+    console.log("Number of students ->", students.length);
+    res.json(students);
+  })
+  .catch((error) => {
+    console.error("Error while retrieving students ->", error);
+    res.status(500).send({ error: "Failed to retrieve students" });
+  });
 });
 
 
-// START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
