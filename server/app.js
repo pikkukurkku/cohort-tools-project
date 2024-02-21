c2c3007dfade162a9e0f8a7a5bbf649ad627697f
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-21 19:06:14 +0100

    finished all get routes

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const PORT = 5005;
 const cors = require("cors");
 const Student = require("./models/Student.model");
 const Cohort = require("./models/Cohort.model");
-
+const ObjectId = mongoose.Types.ObjectId;
 
 const app = express();
 
@@ -46,6 +46,28 @@ app.get("/api/cohorts", cors(), (req, res) => {
   });
 });
 
+app.get("/api/cohorts/:cohortId", cors(), (req, res) => {
+  const cohortId = req.params.cohortId;
+
+  if (!ObjectId.isValid(cohortId)) {
+    return res.status(400).json({ error: "Invalid cohort ID format" });
+  }
+
+  Cohort.findById(cohortId)
+  .then((cohort) => {
+    if (!cohort) {
+      return res.status(404).json({ error: "Cohort not found" });
+    }
+
+    console.log("Retrieved cohort by ID ->", cohort);
+    res.json(cohort);
+  })
+  .catch((error) => {
+    console.error("Error while retrieving cohort by ID ->", error);
+    res.status(500).send({ error: "Failed to retrieve cohort by ID" });
+  });
+});
+
 app.get("/api/students", cors(), (req, res) => {
   Student.find({})
   .then((students)=> {
@@ -58,6 +80,46 @@ app.get("/api/students", cors(), (req, res) => {
     res.status(500).send({ error: "Failed to retrieve students" });
   });
 });
+
+app.get("/api/students/cohort/:cohortId", cors(), (req, res) => {
+  const cohortId = req.params.cohortId;
+  if (!ObjectId.isValid(cohortId)) {
+    return res.status(400).json({ error: "Invalid cohort ID format" });
+  }
+  Student.find({cohort: cohortId})
+  .then((students)=> {
+    console.log("Retrieved students for cohort ->", students);
+    console.log("Number of students ->", students.length);
+    res.json(students);
+  })
+  .catch((error) => {
+    console.error("Error while retrieving students ->", error);
+    res.status(500).send({ error: "Failed to retrieve students" });
+  });
+});
+
+app.get("/api/students/:studentId", cors(), (req, res) => {
+  const studentId = req.params.studentId;
+
+  if (!ObjectId.isValid(studentId)) {
+    return res.status(400).json({ error: "Invalid student ID format" });
+  }
+
+  Student.findById(studentId)
+  .then((student) => {
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+
+    console.log("Retrieved student by ID ->", student);
+    res.json(student);
+  })
+  .catch((error) => {
+    console.error("Error while retrieving student by ID ->", error);
+    res.status(500).send({ error: "Failed to retrieve student by ID" });
+  });
+});
+
 
 
 app.listen(PORT, () => {
